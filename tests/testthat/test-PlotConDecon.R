40e592b4b687753eea47cc917d674aa0d447b3d8
Rachael Aubin <rach226a@gmail.com>
2023-01-09 18:00:17 -0500

    Add test for PlotConDecon

---
+++ tests/testthat/test-PlotConDecon.R
@@ -0,0 +1,20 @@
+# Tests for functions in PlotConDecon.R
+# ------------------------------------------------------------------------------
+
+data("counts_gps")
+data("latent_gps")
+data("variable_genes_gps")
+data("bulk_gps")
+
+ConDecon_obj = RunConDecon(counts = counts_gps, latent = latent_gps, bulk = bulk_gps,
+                variable.features = variable_genes_gps, dims = 10, max.iter = 50,
+                verbose = FALSE)
+
+data("umap_embedding_gps")
+ConDecon_obj = PlotConDecon(ConDecon_obj = ConDecon_obj, umap = umap_embedding_gps)
+
+test_that("Check that ConDecon's predicted relative cell abundance matrix has the correct dims", {
+  expect_equal(dim(ConDecon_obj$relative_cell.prob), c(nrow(ConDecon_obj$norm_cell.prob), ncol(ConDecon_obj$norm_cell.prob)))
+})
+
+
--- tests/testthat/test-RunConDecon.R
+++ tests/testthat/test-RunConDecon.R
@@ -6,11 +6,11 @@ data("latent_gps")
 data("variable_genes_gps")
 data("bulk_gps")
 
-test_that("Check that dims of ConDecon's predicted cell abundances is correct", {
-  x = RunConDecon(counts = counts_gps, latent = latent_gps, bulk = bulk_gps,
+test_that("Check that ConDecon's predicted cell abundance matrix has the correct dims", {
+  ConDecon_obj = RunConDecon(counts = counts_gps, latent = latent_gps, bulk = bulk_gps,
                   variable.features = variable_genes_gps, dims = 10, max.iter = 50,
                   verbose = FALSE)
-  expect_equal(dim(x$norm_cell.prob), c(ncol(counts_gps), ncol(bulk_gps)))
+  expect_equal(dim(ConDecon_obj$norm_cell.prob), c(ncol(counts_gps), ncol(bulk_gps)))
 })
 
 
