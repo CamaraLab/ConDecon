8a072ef944aba692a7b630ed245481c8bc1c188b
Rachael Aubin <rach226a@gmail.com>
2023-01-18 17:16:24 -0500

    update output names in ConDecon object

--- tests/testthat/test-RunConDecon.R
+++ tests/testthat/test-RunConDecon.R
@@ -10,7 +10,7 @@ test_that("Check that ConDecon's predicted cell abundance matrix has the correct
   ConDecon_obj = RunConDecon(counts = counts_gps, latent = latent_gps, bulk = bulk_gps,
                   variable.features = variable_genes_gps, dims = 10, max.iter = 50,
                   verbose = FALSE)
-  expect_equal(dim(ConDecon_obj$norm_cell.prob), c(ncol(counts_gps), ncol(bulk_gps)))
+  expect_equal(dim(ConDecon_obj$Normalized_cell.prob), c(ncol(counts_gps), ncol(bulk_gps)))
 })
 
 
