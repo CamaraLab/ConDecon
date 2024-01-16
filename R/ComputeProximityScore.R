287da5f53f6029a21b00bb6ee671c7c55fac7194
Rachael Aubin <rach226a@gmail.com>
2024-01-15 21:57:37 -0500

    Update ComputeProximityScore and add documentation files

--- R/ComputeProximityScore.R
+++ R/ComputeProximityScore.R
@@ -7,7 +7,7 @@
 #' @return ConDecon object with computed proximity score
 #' @export
 ComputeProximityScore <- function(cond) {
-  euclid_query_train <- as.matrix(pdist((cond$PredictCellProb$bulk_coefficients), t(cond$TrainingSet$bulk_coefficients)))
+  euclid_query_train <- as.matrix(pdist::pdist((cond$PredictCellProb$bulk_coefficients), t(cond$TrainingSet$bulk_coefficients)))
   k <- 10
   knn <- t(apply(euclid_query_train, 1, function(i){
     order(i, decreasing = F)[1:k]
@@ -17,7 +17,7 @@ ComputeProximityScore <- function(cond) {
     euclid_query_train[i[1],i[2:(k+1)]]
   }))
   
-  euclid_train <- as.matrix(pdist(t(cond$TrainingSet$bulk_coefficients), t(cond$TrainingSet$bulk_coefficients)))
+  euclid_train <- as.matrix(pdist::pdist(t(cond$TrainingSet$bulk_coefficients), t(cond$TrainingSet$bulk_coefficients)))
   k <- 11
   knn_train <- t(apply(euclid_train, 1, function(i){
     order(i, decreasing = F)[1:k]
@@ -26,7 +26,7 @@ ComputeProximityScore <- function(cond) {
     euclid_train[i[1],i[2:(k)]]
   }))
   
-  cond$proximity_score <- rowMeans(knn_dist/mean(as.vector(knn_dist_train)))
+  cond$proximity_score <- Matrix::rowMeans(knn_dist/mean(as.vector(knn_dist_train)))
   
   return(cond)
 }
